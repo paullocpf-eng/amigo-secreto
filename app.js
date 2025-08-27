95c815c01f6f79636b4eef22c9a3ddeaa0db522f
Paulo Cesar <paullocpf@gmail.com>
2025-08-27 01:00:09 -0300

    Criando vetor 'amigos' e função que adiciona os nomes

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+//Criação da lista(vetor) "amigos"
+let amigos = []; 
+
+//Função para adicionar o amigo ao clicar no botão "Adicionar"
+function adicionarAmigo() { 
+    const campo = document.getElementById('amigo'); 
+    let nome = campo.value.trim();
+    //IF Verifica se o campo está vazio e exibe uma mensagem de erro se tentar adicionar sem digitar nada
+    if (nome == '') { 
+        alert('Por favor, insira um nome válido');
+        return;
+    } 
+    amigos.push(nome);
+    campo.value = '';
+    
+    console.log(nome);   
+    console.log(amigos);   
+}
