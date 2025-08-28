94bf64c1ec557cf4259665d3e44076ecf7241572
Paulo Cesar <paullocpf@gmail.com>
2025-08-28 14:34:13 -0300

    Criar a função listarAmigos() e a inserir na função adicionarAmigo()

--- app.js
+++ app.js
@@ -16,5 +16,18 @@ function adicionarAmigo() {
     campo.value = '';
     
     console.log(nome);   
-    console.log(amigos);   
+    console.log(amigos);
+    
+    listarAmigos();  //Chama a função que mostra os nomes adicionados na tela 
+}
+
+function listarAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ''; // limpa a lista
+
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement('li');  //Para cada nome é criado um elemento "<li></li> no HTML"
+        li.textContent = amigos[i];  
+        lista.appendChild(li);
+    }
 }
